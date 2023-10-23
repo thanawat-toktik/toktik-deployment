45b837fa7a2f9bad3a640d23be49cfcbbb87c3b2
Nawat Ngerncham <67263644+nngerncham@users.noreply.github.com>
2023-10-23 16:06:55 +0700

    fix: removed rabbitmq from dev scripts

--- dev-scripts/start-test-mq.sh
+++ dev-scripts/start-test-mq.sh
@@ -1,12 +1,6 @@
 #!/bin/bash
 
 docker run \
-    -p "5673:5672" \
-    -p "15673:15672" \
-    -d \
-    rabbitmq:management-alpine
-
-docker run \
     -p "6381:6379" \
     -d \
     redis:alpine
