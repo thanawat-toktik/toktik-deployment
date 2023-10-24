82d5ae21eab523ae6321d4ff24ac9f01d2c592bf
Nawat Ngerncham <67263644+nngerncham@users.noreply.github.com>
2023-10-25 02:25:14 +0700

    feat: volume for dev script

--- dev-scripts/start-test-db.sh
+++ dev-scripts/start-test-db.sh
@@ -1,7 +1,11 @@
 #!/bin/bash
 
+if [ ! $(docker volume ls | grep -q "postgres-vol") ]; then
+    docker volume create "postgres-vol"
+fi
 docker run -d -p 5433:5432 \
     --env POSTGRES_USER=postgres \
     --env POSTGRES_PASSWORD=password \
     --env POSTGRES_DB=postgres \
+    -v postgres-vol:/var/lib/postgresql/data \
     postgres:alpine
